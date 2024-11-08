bd9388b55503f03003bbfee77da4503c5fa8a0a7
Ian Joseph Navarro <navarroian1991@gmail.com>
2024-11-08 13:57:51 +0800

    ses provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Mail;
+use Aws\Ses\SesClient;
+use Illuminate\Mail\Transport\SesTransport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +22,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $sesClient = new SesClient([
+            'version' => 'latest',
+            'region'  => 'ap-northeast-1', // Explicitly force Tokyo region for SES
+            'credentials' => [
+                'key'    => env('AWS_ACCESS_KEY_ID'),
+                'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            ],
+        ]);
+    
+        Mail::extend('ses', function() use ($sesClient) {
+            return new SesTransport($sesClient);
+        });
     }
 }
