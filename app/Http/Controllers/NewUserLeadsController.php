634187c809aaf9569f1d42b922d09795acca2862
some3L <luckyllemos0909@gmail.com>
2026-02-27 04:57:06 +0800

    Update add new controller for newUserLeads

---
+++ app/Http/Controllers/NewUserLeadsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\NewUserLeads;
+
+class NewUserLeadsController extends Controller
+{
+
+    public function save_email(Request $request)
+    {
+
+        if(!$request->first_name || !$request->last_name || !$request->email)
+        {
+            return response()->json([
+                'status'=>false,
+                'message'=>'First name, last name and email required'
+            ]);
+        }
+
+        $exists = NewUserLeads::where('email',$request->email)->first();
+
+        if($exists)
+        {
+            return response()->json([
+                'status'=>true,
+                'message'=>'Email already saved'
+            ]);
+        }
+
+        NewUserLeads::create([
+            'first_name'=>$request->first_name,
+            'last_name'=>$request->last_name,
+            'email'=>$request->email
+        ]);
+
+        return response()->json([
+            'status'=>true,
+            'message'=>'User details saved successfully'
+        ]);
+
+    }
+
+}
